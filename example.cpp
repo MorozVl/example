cd77cabe1e6bbb1961cbf64ae6428378a9738403
Moroz Vladimir <v_moroz@hotmail.com>
2016-04-08 17:03:15 +0300

    test

---
+++ example.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	/* code */
+	const int PARAM = 2;
+	int a, b;
+	a= 5;
+	b = a * PARAM;
+
+	cout << "b = " << b << endl;
+	return 0;
+}
