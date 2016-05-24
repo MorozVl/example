10e78d76534672b86044e9c5b2cc1b52ce82e787
Vladimir <V_moroz@hotmail.com>
2016-05-24 22:55:31 +0300

    static variable

--- Banner.cpp
+++ Banner.cpp
@@ -9,13 +9,20 @@ using namespace std;
 class Banner  	//Имя класса
 { 				// начало определения класса
 	public: string favoriteProgram;  //переменная-член
+	public: static int numberOfBannerObject;
 	public:	Banner(){
 		cout <<"Banner's constructor" << endl;
+		numberOfBannerObject++;
 		favoriteProgram = "C++";
 	}
 	public: Banner(string param1){
 		cout << "Banner's Overloaded Constractor" << endl;
+		numberOfBannerObject++;
 		favoriteProgram = param1;
+	}
+	public: void HowMany(){
+		cout << "The nuber of Banner objects is " <<
+			numberOfBannerObject << endl;
 	}
 	public: void Display(){			//поведение класса
 		cout << "I Love " << favoriteProgram << endl;
